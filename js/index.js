6b022eb64c5db4fbaa0a82eda50a823f7f631182
SuperCat2812 <Sashka281205@gmail.com>
2021-09-09 15:51:38 +0300

    main

---
+++ js/index.js
@@ -0,0 +1,8 @@
+let name = prompt('What is your name?');
+let age1 = +prompt('Your birth year?');
+let age2 = +prompt('Year now');
+let b = 0;
+b = age2 - age1;
+console.log(b);
+console.log(name);
+alert(name+'-'+b)
